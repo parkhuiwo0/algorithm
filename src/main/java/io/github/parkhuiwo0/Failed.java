cb27e0b89f3ea06c55428c3383074fe46909c743
parkhuiwo0 <huiwo0@naver.com>
2023-03-08 11:36:01 +0900

    Add marker annotation failed

---
+++ src/main/java/io/github/parkhuiwo0/Failed.java
@@ -0,0 +1,15 @@
+package io.github.parkhuiwo0;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * failed problem marker interface.
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+public @interface Failed {
+
+}
