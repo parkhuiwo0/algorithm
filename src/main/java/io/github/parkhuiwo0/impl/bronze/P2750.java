978450984a3da3bc7e346c90c3d0cc6b131013c8
parkhuiwo0 <huiwo0@naver.com>
2023-10-30 01:15:45 +0900

    Solved 2750

---
+++ src/main/java/io/github/parkhuiwo0/impl/bronze/P2750.java
@@ -0,0 +1,26 @@
+package io.github.parkhuiwo0.impl.bronze;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class P2750 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        int[] nums = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            nums[i] = Integer.parseInt(br.readLine());
+        }
+
+        Arrays.sort(nums);
+
+        for (int num : nums) {
+            System.out.println(num);
+        }
+    }
+}
