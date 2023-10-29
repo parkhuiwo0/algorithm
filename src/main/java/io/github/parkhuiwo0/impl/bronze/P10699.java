d3c6814d2be2c399995540e3d290cf97e9869681
parkhuiwo0 <huiwo0@naver.com>
2023-10-30 00:36:18 +0900

    Solved 10699

---
+++ src/main/java/io/github/parkhuiwo0/impl/bronze/P10699.java
@@ -0,0 +1,10 @@
+package io.github.parkhuiwo0.impl.bronze;
+
+import java.time.LocalDate;
+import java.time.ZoneId;
+
+public class P10699 {
+    public static void main(String[] args) {
+        System.out.println(LocalDate.now(ZoneId.of("Asia/Seoul")));
+    }
+}
