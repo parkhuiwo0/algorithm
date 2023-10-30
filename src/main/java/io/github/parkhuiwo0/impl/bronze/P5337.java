a65df703835ece1fde257f09ed283a4485e4519c
parkhuiwo0 <huiwo0@naver.com>
2023-10-30 10:46:49 +0900

    Solved 5337

---
+++ src/main/java/io/github/parkhuiwo0/impl/bronze/P5337.java
@@ -0,0 +1,9 @@
+package io.github.parkhuiwo0.impl.bronze;
+
+public class P5337 {
+    public static void main(String[] args) {
+        System.out.println(".  .   .");
+        System.out.println("|  | _ | _. _ ._ _  _");
+        System.out.println("|/\\|(/.|(_.(_)[ | )(/.");
+    }
+}
