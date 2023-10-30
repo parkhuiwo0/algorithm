dffeac08f0ef24ad1ba43c1ba9bdb3dae21f8499
parkhuiwo0 <huiwo0@naver.com>
2023-10-30 14:13:52 +0900

    Solved 11719

---
+++ src/main/java/io/github/parkhuiwo0/impl/bronze/P11719.java
@@ -0,0 +1,15 @@
+package io.github.parkhuiwo0.impl.bronze;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class P11719 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s;
+        while ((s = br.readLine()) != null) {
+            System.out.println(s);
+        }
+    }
+}
