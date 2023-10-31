e95ff8330f34863d0f0c6e4a4353950b555e5aa4
parkhuiwo0 <huiwo0@naver.com>
2023-10-31 23:19:48 +0900

    Solved 2739

---
+++ src/main/java/io/github/parkhuiwo0/impl/bronze/P2739.java
@@ -0,0 +1,16 @@
+package io.github.parkhuiwo0.impl.bronze;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class P2739 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        for (int i = 1; i <= 9; i++) {
+            System.out.printf("%d * %d = %d\n", n, i, n*i);
+        }
+    }
+}
