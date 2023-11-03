a3daf5b34f4d87afa9aae65c73356aece73cdcdd
parkhuiwo0 <huiwo0@naver.com>
2023-11-04 01:07:34 +0900

    Solved 4158

---
+++ src/main/java/io/github/parkhuiwo0/binarysearch/P4158.java
@@ -0,0 +1,45 @@
+package io.github.parkhuiwo0.binarysearch;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class P4158 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int count = 0;
+        int[] nNums;
+        int n = 0;
+        int m = 0;
+        while (true) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            n = Integer.parseInt(st.nextToken());
+            m = Integer.parseInt(st.nextToken());
+
+            if (n == 0 && m == 0) {
+                break;
+            }
+
+            nNums = new int[n];
+
+            for (int i = 0; i < n; i++) {
+                nNums[i] = Integer.parseInt(br.readLine());
+            }
+
+            for (int i = 0; i < m; i++) {
+                if (Arrays.binarySearch(nNums, Integer.parseInt(br.readLine())) >= 0) {
+                    count++;
+                }
+            }
+
+            System.out.println(count);
+
+            count = 0;
+            n = 0;
+            m = 0;
+        }
+    }
+}
