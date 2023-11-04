1ceacd46fd8a45b149d65a21c68f41b11a10faff
parkhuiwo0 <huiwo0@naver.com>
2023-11-05 01:34:56 +0900

    Solved 10926

---
+++ src/main/java/io/github/parkhuiwo0/impl/bronze/P10926.java
@@ -0,0 +1,15 @@
+package io.github.parkhuiwo0.impl.bronze;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class P10926 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String s = br.readLine();
+
+        System.out.println(s + "??!");
+    }
+}
