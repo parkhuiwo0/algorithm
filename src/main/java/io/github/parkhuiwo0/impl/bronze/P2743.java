a8100f81cfe131bf5c0c78ff90a32633b12fd15e
parkhuiwo0 <huiwo0@naver.com>
2023-11-07 23:18:01 +0900

    Solved 2743

---
+++ src/main/java/io/github/parkhuiwo0/impl/bronze/P2743.java
@@ -0,0 +1,15 @@
+package io.github.parkhuiwo0.impl.bronze;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class P2743 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        String s = bufferedReader.readLine();
+
+        System.out.println(s.length());
+    }
+}
