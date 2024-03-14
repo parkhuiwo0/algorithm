f15fb345fc72081c3329b97b7c592e192327a3d7
parkhuiwo0 <huiwo0@naver.com>
2024-03-14 23:07:32 +0900

    Solved 11718

---
+++ src/main/java/io/github/parkhuiwo0/impl/bronze/P11718.java
@@ -0,0 +1,17 @@
+package io.github.parkhuiwo0.impl.bronze;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class P11718 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String line = "";
+
+        while((line = br.readLine()) != null) {
+            System.out.println(line);
+        }
+    }
+}
