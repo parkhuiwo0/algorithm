ce28cb57cd6f86dcbcd9ad7caab2c822e574eb99
parkhuiwo0 <huiwo0@naver.com>
2024-03-14 22:58:34 +0900

    Solved 10950

---
+++ src/main/java/io/github/parkhuiwo0/impl/bronze/P10950.java
@@ -0,0 +1,19 @@
+package io.github.parkhuiwo0.impl.bronze;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class P10950 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int count = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < count; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            System.out.println(Integer.parseInt(st.nextToken()) + Integer.parseInt(st.nextToken()));
+        }
+    }
+}
