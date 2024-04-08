43864c241735bfefdd5044c0e044a84ee9646b88
parkhuiwo0 <huiwo0@naver.com>
2024-04-08 23:04:19 +0900

    Add InsertionSorting

---
+++ src/main/java/sorting/InsertionSorting.java
@@ -0,0 +1,23 @@
+package sorting;
+
+public class InsertionSorting {
+    public static void main(String[] args) {
+        int[] array = {2, 1, 6, 3, 4, 8, 9};
+
+        for (int i = 1; i < array.length; i++) {
+            int temp = array[i];
+            int j = i - 1;
+
+            while (j >= 0 && array[j] > temp) {
+                array[j + 1] = array[j];
+                j--;
+            }
+
+            array[j+1] = temp;
+        }
+
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + " ");
+        }
+    }
+}
