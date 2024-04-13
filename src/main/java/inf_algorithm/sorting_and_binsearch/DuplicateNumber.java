c8f912e0bb3390107bdfda64586e84b9caff1366
parkhuiwo0 <huiwo0@naver.com>
2024-04-13 22:37:00 +0900

    Solved DuplicateNumber

---
+++ src/main/java/inf_algorithm/sorting_and_binsearch/DuplicateNumber.java
@@ -0,0 +1,33 @@
+package inf_algorithm.sorting_and_binsearch;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class DuplicateNumber {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+
+        for (int i = 0; i < arr.length - 1; i++) {
+            if (arr[i] == arr[i + 1]) {
+                System.out.print("D");
+                return;
+            }
+        }
+
+        System.out.print("U");
+    }
+}
