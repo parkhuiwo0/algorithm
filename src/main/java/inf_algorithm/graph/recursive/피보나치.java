3fbd313fd9ce1b73798c6d00654e35c2ee4ffe01
parkhuiwo0 <huiwo0@naver.com>
2024-04-14 17:56:52 +0900

    Solved 피보나치2

--- src/main/java/inf_algorithm/graph/recursive/피보나치.java
+++ src/main/java/inf_algorithm/graph/recursive/피보나치.java
@@ -1,16 +1,21 @@
 package inf_algorithm.graph.recursive;
 
 public class 피보나치 {
+    static int[] arr;
 
     static int dfs(int n) {
         if (n == 1 || n == 2) {
-            return 1;
+            return arr[n] = 1;
         }
 
-        return dfs(n-2) + dfs(n-1);
+        return arr[n] = dfs(n-2) + dfs(n-1);
     }
 
     public static void main(String[] args) {
+        arr = new int[11];
         dfs(10);
+        for (int i = 1; i<=10; i++) {
+            System.out.println(arr[i] + " ");
+        }
     }
 }
